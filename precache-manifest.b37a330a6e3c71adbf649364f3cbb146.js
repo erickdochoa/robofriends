b0b8b690f9dd96292bfadcb189616e92bc711897
erickdochoa <ochoa.erick.d@gmail.com>
2019-01-13 14:53:52 -0800

    Updates

---
+++ precache-manifest.b37a330a6e3c71adbf649364f3cbb146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "730c4611db4fa14f8861",
+    "url": "/robofriends/static/js/main.730c4611.chunk.js"
+  },
+  {
+    "revision": "842b7d1afd583784d04b",
+    "url": "/robofriends/static/js/1.842b7d1a.chunk.js"
+  },
+  {
+    "revision": "730c4611db4fa14f8861",
+    "url": "/robofriends/static/css/main.750e172d.chunk.css"
+  },
+  {
+    "revision": "842b7d1afd583784d04b",
+    "url": "/robofriends/static/css/1.459f7fbe.chunk.css"
+  },
+  {
+    "revision": "8e119be57dddc60ccaf289abd020b98a",
+    "url": "/robofriends/index.html"
+  }
+];
